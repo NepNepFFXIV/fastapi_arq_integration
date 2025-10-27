834238a0b85c7d298f3f794f6d62ae6f46f5b3a2
Nep Nep <compsci88@gmail.com>
2025-10-27 05:28:17 -0700

    integrate db operation to arq functions

--- src/fastapi_arq_integration/worker/functions/insert_operation.py
+++ src/fastapi_arq_integration/worker/functions/insert_operation.py
@@ -1,2 +1,6 @@
-async def insert_operation(ctx, description: str):
-    print(f"insert_operation: {description}")
+from src.fastapi_arq_integration.worker.worker_context import WorkerContext
+
+
+async def insert_operation(ctx: WorkerContext, description: str):
+    repository = ctx["repository"]
+    await repository.insert_operation(description)
